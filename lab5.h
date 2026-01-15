c0e47cc9beb08a0f56d5940c72f7b528fd608796
SaveliyShutov <savelijsutov@gmail.com>
2026-01-15 10:41:22 +0500

    fix

--- lab5.h
+++ lab5.h
@@ -14,46 +14,44 @@ struct Toy {
 };
 
 class Lab5 {
- public:
-  static void GenerateBinaryNumbers(const std::string& filename,
-                                    std::size_t count,
-                                    int min_value = -100,
+public:
+  static void GenerateBinaryNumbers(const std::string &filename,
+                                    std::size_t count, int min_value = -100,
                                     int max_value = 100);
 
   static void GenerateBinaryToys(
-      const std::string& filename,
-      const std::vector<std::tuple<std::string, int, int, int>>& toys);
+      const std::string &filename,
+      const std::vector<std::tuple<std::string, int, int, int>> &toys);
 
-  static void GenerateTextIntegersOnePerLine(const std::string& filename,
+  static void GenerateTextIntegersOnePerLine(const std::string &filename,
                                              std::size_t count,
                                              int min_value = -100,
                                              int max_value = 100);
 
-  static void GenerateTextIntegersMultiplePerLine(const std::string& filename,
-                                                   std::size_t count,
-                                                   int numbers_per_line = 5,
-                                                   int min_value = -100,
-                                                   int max_value = 100);
+  static void GenerateTextIntegersMultiplePerLine(const std::string &filename,
+                                                  std::size_t count,
+                                                  int numbers_per_line = 5,
+                                                  int min_value = -100,
+                                                  int max_value = 100);
 
-  static void GenerateTextFileWithLines(const std::string& filename,
-                                        const std::vector<std::string>& lines);
+  static void GenerateTextFileWithLines(const std::string &filename,
+                                        const std::vector<std::string> &lines);
 
-  static void Task1_CumulativePrefixes(const std::string& src,
-                                       const std::string& dest);
+  static void Task1_CumulativePrefixes(const std::string &src,
+                                       const std::string &dest);
 
-  static std::size_t Task2_MatrixClosestRow(const std::string& src,
+  static std::size_t Task2_MatrixClosestRow(const std::string &src,
                                             std::size_t n);
 
-  static std::vector<std::string> Task3_ToysForAges(const std::string& src,
-                                                    int age1 = 4,
-                                                    int age2 = 10);
+  static std::vector<std::string>
+  Task3_ToysForAges(const std::string &src, int age1 = 4, int age2 = 10);
 
-  static long long Task4_ProductMinMax(const std::string& filename);
+  static long long Task4_ProductMinMax(const std::string &filename);
 
-  static std::size_t Task5_CountOdd(const std::string& filename);
+  static std::size_t Task5_CountOdd(const std::string &filename);
 
-  static void Task6_LineLengths(const std::string& src,
-                                const std::string& dest);
+  static void Task6_LineLengths(const std::string &src,
+                                const std::string &dest);
 };
 
-#endif  // LAB5_H_
+#endif // LAB5_H_
